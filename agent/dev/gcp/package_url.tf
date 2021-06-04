ba7007fa83e622680d54e7cfe56315c5c1b8f3f1
Ivan Kovalkovskyi <vaniakov95@gmail.com>
2021-06-04 19:30:22 +0300

    Install pyopenssl.

--- agent/dev/gcp/package_url.tf
+++ agent/dev/gcp/package_url.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "null_resource" "latest_package_url" {
   provisioner "local-exec" {
-    command = "gsutil signurl -d 1d -u $(gsutil ls -l  gs://${var.gcp_bucket}/omnibus/pkg/${var.branch}/el/7/scalr-agent-*.rpm | sort -k 2 | tail -n 2 | head -1 | cut -d ' ' -f 6) > ${local.filename}"
+    command = "pip install pyopenssl && gsutil signurl -d 1d -u $(gsutil ls -l  gs://${var.gcp_bucket}/omnibus/pkg/${var.branch}/el/7/scalr-agent-*.rpm | sort -k 2 | tail -n 2 | head -1 | cut -d ' ' -f 6) > ${local.filename}"
   }
   triggers = {
     keep = formatdate("DD-MM-YYYY", timestamp())
